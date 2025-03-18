be223124c24c1ecdc7fd4a07e7ea4d0712689ffc
Oussama Khouya <ouskhouya@gmail.com>
2025-03-18 18:01:26 +0000

    book form add with image and video upload+ display books on Home page from DB

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import BookCover from "@/components/BookCover";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
-const BookOverview = ({
+interface Props extends Book {
+  userId: string;
+}
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -13,19 +18,37 @@ const BookOverview = ({
   description,
   coverColor,
   coverUrl,
-}: Book) => {
+  id,
+  userId,
+}: Props) => {
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, userId))
+    .limit(1);
+
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
+    message:
+      availableCopies <= 0
+        ? "Book is not available"
+        : "You are not eligible to borrow this book",
+  };
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+
         <div className="book-info">
           <p>
             By <span className="font-semibold text-light-200">{author}</span>
           </p>
+
           <p>
             Category{" "}
             <span className="font-semibold text-light-200">{genre}</span>
           </p>
+
           <div className="flex flex-row gap-1">
             <Image src="/icons/star.svg" alt="star" width={22} height={22} />
             <p>{rating}</p>
@@ -43,11 +66,6 @@ const BookOverview = ({
         </div>
 
         <p className="book-description">{description}</p>
-
-        <Button className="book-overview_btn">
-          <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-          <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
-        </Button>
       </div>
 
       <div className="relative flex flex-1 justify-center">
@@ -71,4 +89,5 @@ const BookOverview = ({
     </section>
   );
 };
+
 export default BookOverview;
